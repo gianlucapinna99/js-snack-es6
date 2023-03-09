ea9292ddaa94b17413d043b9aad17491b59c8c71
gianlucapinna99 <gianlucapinna99@gmail.com>
2023-03-09 16:49:10 +0100

    For cycle to find lightest bike

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,1 +1,16 @@
 "use strict"
+
+const bikes = [
+  { nome: 'Bianchi', peso: 7 },
+  { nome: 'Decathlon', peso: 5 },
+  { nome: 'Shimano', peso: 6.5 },
+  { nome: 'Cannondale', peso: 7.5 }
+];
+
+const lightestBike = bikes.reduce((acc, bike) => {
+    if (bike.peso < acc.peso) {
+      return bike;
+    } else {
+      return acc;
+    }
+  });
